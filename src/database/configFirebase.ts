e4b8587c5cd4de42367a29ab70b215d312baa16e
David <49219152+DavidSchneiter@users.noreply.github.com>
2023-01-26 23:30:17 -0300

    Update configFirebase.ts

--- src/database/configFirebase.ts
+++ src/database/configFirebase.ts
@@ -1,19 +1,19 @@
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
-import {getFirestore} from 'firebase-admin/firestore'
-import { logger } from '../utils';
-import serviceAccount from './adminFirebase.json';
+// import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+// import {getFirestore} from 'firebase-admin/firestore'
+// import { logger } from '../utils';
+// import serviceAccount from './adminFirebase.json';
 
-initializeApp({
-  credential: cert(serviceAccount as ServiceAccount)
-});
+// initializeApp({
+//   credential: cert(serviceAccount as ServiceAccount)
+// });
 
-export const dbConnectionFirebase = async() => {
- try {
-   const db = getFirestore()
-   logger.info('Firestore connection')
-   return db
- } catch (error) {
-   logger.error(error)
-   throw new Error("Firestore connection error: " + error)
- }
-}
+// export const dbConnectionFirebase = async() => {
+//  try {
+//    const db = getFirestore()
+//    logger.info('Firestore connection')
+//    return db
+//  } catch (error) {
+//    logger.error(error)
+//    throw new Error("Firestore connection error: " + error)
+//  }
+// }
